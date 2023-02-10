3b0da530c7faf2c96bbcfbf16e5322a1738bd092
CaptainHaider <m.hassanhaider@hotmail.com>
2023-02-11 00:24:01 +0500

    first commit

---
+++ Components/EnvVars.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { View, Button, Text } from 'native-base';
+import { SPOTIFY_REDIRECT_URL, SPOTIFY_TOKEN_REFRESH_URL, SPOTIFY_TOKEN_SWAP_URL } from 'react-native-dotenv';
+import styles from '../styles';
+
+export default function EnvVars () {
+    const [show, setShow] = useState(false);
+    return (
+      <View>
+       
+      </View>
+    )
+}
